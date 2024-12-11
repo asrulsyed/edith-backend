4c206bee7f670a587e874916e975174742ae3b30
Dane Manji <danemanji0089@gmail.com>
2024-12-10 18:31:32 -0600

    update

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,7 @@
 module.exports = {
     port: 3500,
     db: "mongodb+srv://jacksonchen1103:ZB0J0Scy28Jx38tx@cluster0.e1l5i.mongodb.net",
-    secret: "CHATEDITHKEY"
+    secret: "CHATEDITHKEY",
+    client_url: "https://edith-react.vercel.app",
+    server_url: "https://edith-backend.vercel.app",
 }
